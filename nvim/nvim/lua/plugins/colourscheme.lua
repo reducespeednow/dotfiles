c1041b0fed32778e53bab77d19309e2abe3a909c
Sara Abdullahi <sara.abdullahi.22@ucl.ac.uk>
2025-12-14 18:32:29 +0000

    nvim opaque bkg, rust formatter, remove padding in nvim

--- nvim/nvim/lua/plugins/colourscheme.lua
+++ nvim/nvim/lua/plugins/colourscheme.lua
@@ -1,10 +1,8 @@
 return {
-	"rose-pine/neovim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd("colorscheme rose-pine")
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	end,
+    "rose-pine/neovim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.cmd("colorscheme rose-pine")
+    end,
 }

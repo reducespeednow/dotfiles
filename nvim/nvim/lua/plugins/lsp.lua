ebe42873bcb2f7d801335608a8520f1ea6a14b96
Sara Abdullahi <sara.abdullahi.22@ucl.ac.uk>
2025-12-20 16:27:41 +0000

    added useful keybinds for lsp

--- nvim/nvim/lua/plugins/lsp.lua
+++ nvim/nvim/lua/plugins/lsp.lua
@@ -24,6 +24,32 @@ return {
         require("fidget").setup({})
         require("mason").setup()
 
+        vim.diagnostic.config({
+            virtual_text = true,
+            signs = true,
+            underline = true,
+            update_in_insert = false,
+            severity_sort = true,
+        })
+
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+            callback = function(ev)
+                local opts = { buffer = ev.buf }
+
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                vim.keymap.set('n', '<leader>vws', vim.lsp.buf.workspace_symbol, opts)
+                vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, opts)
+                vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
+                vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
+                vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, opts)
+                vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, opts)
+                vim.keymap.set('n', '<leader>vrn', vim.lsp.buf.rename, opts)
+                vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
+            end,
+        })
+
         require('mason-lspconfig').setup({
             automatic_installation = true,
             handlers = {
@@ -62,14 +88,11 @@ return {
             sources = {
                 { name = 'path' },
                 { name = 'nvim_lsp' },
-                { name = 'luasnip', keyword_length = 2 },
-                { name = 'buffer',  keyword_length = 3 },
+                { name = 'luasnip' },
+                { name = 'buffer' },
             },
             mapping = cmp.mapping.preset.insert({
-                -- ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                -- ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                 ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                -- ['<C-Space>'] = cmp.mapping.complete(),
             }),
             snippet = {
                 expand = function(args)

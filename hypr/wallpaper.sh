8eceb413ef9921e9f8d080ab6ec01ed43a7e89d6
Sara Abdullahi <sara.abdullahi.22@ucl.ac.uk>
2026-01-12 16:53:58 +0000

    changed path for wallpapers

--- hypr/wallpaper.sh
+++ hypr/wallpaper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sleep 1
-WALLPAPER_DIR="$HOME/dotfiles/hypr/hypr/wallpaper/"
+WALLPAPER_DIR="$HOME/dotfiles/hypr/wallpaper/"
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
 hyprctl hyprpaper preload "$WALLPAPER"
 hyprctl hyprpaper wallpaper ",$WALLPAPER"

975d1e85158e4aeb01db7cae82ab9d8d30d6c3e1
Sara Abdullahi <sara.abdullahi.22@ucl.ac.uk>
2026-06-20 18:32:48 +0100

    new treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,22 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = {
-                enable = true
-            }
+        require("nvim-treesitter").setup()
+        local ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", }
+        require('nvim-treesitter').install(ensure_installed)
+        local ts_group = vim.api.nvim_create_augroup("vim-treesitter-start", { clear = true })
+        vim.api.nvim_create_autocmd("FileType", {
+            group = ts_group,
+            callback = function()
+                pcall(vim.treesitter.start)
+            end,
+        })
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function()
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
         })
     end,
 }

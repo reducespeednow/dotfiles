76171b83cc80885b8ec99585625fa34d1cf49476
Sara Abdullahi <sara.abdullahi.22@ucl.ac.uk>
2026-07-14 12:52:14 +0100

    updated remaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,4 @@
-require("saraabdullahi.lazy")
-require("saraabdullahi.remap")
 require("saraabdullahi.set")
-
-vim.api.nvim_create_autocmd("VimEnter", {
-    callback = function()
-        vim.defer_fn(function()
-            vim.cmd("silent !kitty @ set-spacing padding=0")
-        end, 100)
-    end,
-})
-
-vim.api.nvim_create_autocmd("VimLeave", {
-    callback = function()
-        vim.cmd("silent !kitty @ set-spacing padding=20")
-    end,
-})
+require("saraabdullahi.remap")
+require("saraabdullahi.lazy")
+require("saraabdullahi.autocmds")
--- nvim/lua/saraabdullahi/set.lua
+++ nvim/lua/saraabdullahi/set.lua
@@ -1,5 +1,5 @@
 vim.opt.guicursor = ""
-vim.opt.nu = true
+vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
@@ -13,17 +13,19 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+local undodir = vim.fn.stdpath("state") .. "/undo"
+vim.fn.mkdir(undodir, "p")
+vim.opt.undodir = undodir
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
-vim.opt.termguicolors = true
-
 vim.opt.colorcolumn = "80"

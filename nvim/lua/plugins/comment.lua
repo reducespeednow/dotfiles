ffcdef4396042943b8b18a55cec7ca1467307cf6
Sara Abdullahi <sara.abdullahi.22@ucl.ac.uk>
2026-07-29 15:51:08 +0100

    removed readme

--- nvim/lua/plugins/comment.lua
+++
@@ -1,8 +0,0 @@
--- gcc to comment line, gc to comment highlighted area, gc{value} to comment that
-return {
-	"numToStr/Comment.nvim",
-	event = "VeryLazy",
-	config = function()
-		require("Comment").setup()
-	end,
-}

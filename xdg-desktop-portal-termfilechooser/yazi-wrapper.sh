9f0f8b58df2fafa64fc9aba4c193d3bc40c948b6
Sara Abdullahi <sara.abdullahi.22@ucl.ac.uk>
2026-08-12 21:59:02 +0100

    screenshares correctly, floating file chooser

--- xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
+++ xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env sh
+multiple="$1"
+directory="$2"
+save="$3"
+path="$4"
+out="$5"
 
-export WAYLAND_DISPLAY=$(systemctl --user show-environment | grep '^WAYLAND_DISPLAY=' | cut -d= -f2)
-export DISPLAY=$(systemctl --user show-environment | grep '^DISPLAY=' | cut -d= -f2)
+for var in WAYLAND_DISPLAY DISPLAY; do
+    val=$(systemctl --user show-environment | sed -n "s/^$var=//p")
+    [ -n "$val" ] && export "$var=$val"
+done
 
-kitty --class=file_chooser -e yazi --chooser-file="$5" "$4"
+if [ "$save" = "1" ]; then
+    dir=$(dirname -- "$path")
+    [ -d "$dir" ] || dir="$HOME"
+    [ -e "$path" ] || : > "$path" 2>/dev/null
+    set -- --chooser-file="$out" "$dir"
+elif [ "$directory" = "1" ]; then
+    [ -d "$path" ] || path="$HOME"
+    set -- --cwd-file="$out" "$path"
+else
+    [ -e "$path" ] || path="$HOME"
+    set -- --chooser-file="$out" "$path"
+fi
+
+exec kitty --class=file_chooser -e yazi "$@"

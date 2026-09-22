ee9ce1d95249a22053b08a841c2aa4b0d1457d52
Sara Abdullahi <saabdulllahi@gmail.com>
2026-09-22 19:24:34 +0100

    nm applet

--- home.nix
+++ home.nix
@@ -7,6 +7,7 @@ in {
   home.stateVersion = "26.05";
 
   services.swaync.enable = true;
+  services.network-manager-applet.enable = true;
 
   services.wpaperd = {
       enable = true;
@@ -29,10 +30,12 @@ in {
           height = 24;
           modules-left = ["niri/workspaces"];
           modules-center = ["clock"];
-          modules-right = ["pulseaudio" "battery"];
+          modules-right = ["tray" "backlight" "pulseaudio" "battery"];
+          backlight.format = "bri {percent}%";
           clock.format = "{:%a %d %b  %H:%M}";
           pulseaudio.format = "vol {volume}%";
           battery.format = "bat {capacity}%";
+          tray.spacing = 8;
       };
   };
 
@@ -43,7 +46,19 @@ in {
     "tmux".source = link "tmux";
   };
 
+  xdg.configFile."xdg-desktop-portal-termfilechooser/config".text =
+    let
+      picker = pkgs.writeShellScript "yazi-picker" ''
+        # args: multiple directory save path out
+        exec alacritty -e yazi --chooser-file="$5" "$4"
+      '';
+    in ''
+      [filechooser]
+      cmd=${picker}
+      default_dir=$HOME
+    '';
+
   home.packages = with pkgs; [
-    fastfetch neovim gcc ripgrep fd unzip tree-sitter lua-language-server wget swaylock libnotify tmux
+    fastfetch neovim gcc ripgrep fd unzip tree-sitter lua-language-server wget swaylock libnotify tmux brightnessctl
   ];
 }

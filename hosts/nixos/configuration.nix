10814eee54a483026e6e78a6c226d53667208382
Sara Abdullahi <saabdulllahi@gmail.com>
2026-09-26 14:51:47 +0100

    moved wallpapers

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -1,58 +1,72 @@
 { config, lib, pkgs, ... }:
 
 {
-    imports = [ ./theme.nix ];
-  networking.networkmanager.enable = true;
-  time.timeZone = "Europe/London";
+    imports = [
+        ./theme.nix
+        ./hardware-configuration.nix
+    ];
+    networking.networkmanager.enable = true;
+    time.timeZone = "Europe/London";
 
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-  };
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
-  programs.niri.enable = true;
-  programs.fish.enable = true;
-  programs.yazi.enable = true;
+    swapDevices = [{
+      device = "/dev/disk/by-partuuid/5e6f8da9-1c22-4512-b67a-cc7c4578fbb8";
+      randomEncryption.enable = true;
+    }];
 
-  virtualisation.docker.enable = true;
+    networking.hostName = "charlie";
+    system.stateVersion = "26.05"; # Did you read the comment?
 
-  users.users.sara = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "docker" ];
-    shell = pkgs.fish;
-  };
+    services.pipewire = {
+      enable = true;
+      pulse.enable = true;
+    };
 
-  systemd.user.tmpfiles.users.sara.rules = map
-    (d: "L+ %h/.config/${d} - - - - %h/dotfiles/${d}")
-    [ "alacritty" "niri" "fish" "nvim" "tmux" "waybar" "wpaperd" "swaync" "fuzzel" ];
+    programs.niri.enable = true;
+    programs.fish.enable = true;
+    programs.yazi.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    vim
-    playerctl
-    wget
-    git
-    brave
-    alacritty
-    fuzzel
-    zip
-    tree
-    neovim
-    tmux
-    fastfetch
-    gcc
-    ripgrep
-    fd
-    unzip
-    tree-sitter
-    lua-language-server
-    swaylock
-    libnotify
-    brightnessctl
-    waybar
-    swaynotificationcenter
-    wpaperd
-    networkmanagerapplet
-  ];
+    virtualisation.docker.enable = true;
 
-  security.pam.services.swaylock = {};
+    users.users.sara = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" "networkmanager" "docker" ];
+      shell = pkgs.fish;
+    };
+
+    systemd.user.tmpfiles.users.sara.rules = map
+      (d: "L+ %h/.config/${d} - - - - %h/dotfiles/${d}")
+      [ "alacritty" "niri" "fish" "nvim" "tmux" "waybar" "wpaperd" "swaync" "fuzzel" ];
+
+    environment.systemPackages = with pkgs; [
+      vim
+      playerctl
+      wget
+      git
+      brave
+      alacritty
+      fuzzel
+      zip
+      tree
+      neovim
+      tmux
+      fastfetch
+      gcc
+      ripgrep
+      fd
+      unzip
+      tree-sitter
+      lua-language-server
+      swaylock
+      libnotify
+      brightnessctl
+      waybar
+      swaynotificationcenter
+      wpaperd
+      networkmanagerapplet
+    ];
+
+    security.pam.services.swaylock = {};
 }
--- hosts/nixos/hardware-configuration.nix
+++ hosts/nixos/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
@@ -18,15 +18,17 @@
       fsType = "ext4";
     };
 
-  boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-uuid/553ab68a-ddca-462e-a5ad-5c3b6b22ef47";
+  boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-uuid/d16a2e93-373d-40a0-a95f-272addb99024";
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/875C-E14D";
+    { device = "/dev/disk/by-uuid/6E07-108D";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/mapper/dev-disk-byx2dpartuuid-5e6f8da9x2d1c22x2d4512x2db67ax2dcc7c4578fbb8"; }
+    ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
